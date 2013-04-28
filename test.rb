15960de8d58e04e059b6a9cd5aa484dfc4954c87
Francis Irving <francis@flourish.org>
2013-04-28 10:40:33 +0100

    Something which breaks it

--- test.rb
+++ test.rb
@@ -10,3 +10,8 @@ ScraperWiki.save_sqlite(['id'], [
   {'id'=> 30, 'animal'=> 'polar bear', 'awesomeness'=> 99 }
 ], table_name = "foobl")
 
+data = {:set_date=>"2013-04-28", :deal_id=>2493454, :company_name=>"Briefing Media Ltd", :created_at=>"2013-01-15", :updated_at=>"2013-04-25", :price=>0, :price_type=>"fixed", :currency=>"GBP", :duration=>1, :status=>"pending", :status_changed_on=>"2013-04-25", :deal_name=>"809 - Farming data", :category=>"Application Development (p)", :background=>"Just a note to keep track of this opportunity"}
+ScraperWiki.save_sqlite(['set_date, deal_id'], data, table_name="highrise")
+
+
+
